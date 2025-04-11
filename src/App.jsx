f3b34b8211e91feb0f6d620b64fd297585f3917e
Ruth991268 <180922005+Ruth991268@users.noreply.github.com>
2025-04-11 21:23:42 +0300

    Initial push to homr branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import TravelHero from './components/TravelHero';
+import ExploreSection from './components/ExploreSection';
+import WhyChooseUs from './components/WhyChooseUs';
+import TourPartners from './components/TourPartners';
+import TestimonialSection from './components/TestimonialSection';
 
 function App() {
-
   return (
-   <div className="App">
-    lala
-   </div>
-  )
+    <div className="font-sans">
+      <TravelHero />
+      <ExploreSection />
+      <WhyChooseUs />
+      <TourPartners />
+      <TestimonialSection />
+    </div>
+  );
 }
 
-export default App
+export default App;
